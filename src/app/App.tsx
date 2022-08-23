338d11fdf31afeb35bd8887eadfaec156d0d6d6f
biraneves <ubirajara.neves@usp.br>
2022-08-22 23:14:51 -0300

    Interfaces ICalendars e IEvents criadas. Funções de consumo de api para calendários e eventos criadas.

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,4 +1,12 @@
+import {getEventsEndpoint} from "../backend";
+
 function App() {
+  getEventsEndpoint().then(events => {
+    for (const event of events) {
+      console.log(event);
+    }
+  });
+
   return <div>Projeto base</div>;
 }
 
