83e6c196b5dbd678d3623e6c76ee0a526155eb4b
biraneves <ubirajara.neves@usp.br>
2022-08-27 13:25:47 -0300

    Função getToday passa para o arquivo dateFunctions.

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,17 @@
 import {CalendarScreen} from "./CalendarScreen";
+import { BrowserRouter as Router, Switch, Route, Link, Redirect } from 'react-router-dom';
 
 function App() {
-  return <CalendarScreen />;
+    return (
+        <Router>
+            <Switch>
+                <Route path="/calendar/:month">
+                    <CalendarScreen />;
+                </Route>
+                <Redirect to={} />
+            </Switch>
+        </Router>
+    );
 }
 
 export default App;
